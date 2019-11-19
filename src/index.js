f00f1f2be33167d0b40499f2443469205db8c865
Abhay Bhosale <abhay.bhosale38@gmail.com>
2019-11-19 13:37:53 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import App from './App';
-import Form from './Form';
-import Thankyou from './Thankyou';
 import Tasks from './Tasks';
 
 // Import routing components
@@ -13,9 +10,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 ReactDOM.render(
     <Router>
         <div>
-            <Route exact path="/" component={App} />
-            <Route exact path="/login" component={Form} />
-            <Route exact path="/thankyou" component={Thankyou} />
+            <Route exact path="/" component={Tasks} />
             <Route exact path="/tasks" component={Tasks} />
         </div>
     </Router>,
